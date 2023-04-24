d54cbe83723ec4edbef99ebe05a1219195df5c7e
liaoruikang <liaoruikang@vnata.com>
2023-04-24 20:10:41 +0800

    docs: add docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+
+export default defineConfig({
+  title: '@effortless-design/docs',
+  description: '一个轻便的vue3组件库',
+});
